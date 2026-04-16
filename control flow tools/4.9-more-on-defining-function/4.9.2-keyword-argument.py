dc625fe14a9952139e871c022f4b5250153fcede
saurabh <saurabh20tiwari04@gmail.com>
2026-04-16 22:22:44 +0530

    learning functions

--- control flow tools/4.9-more-on-defining-function/4.9.2-keyword-argument.py
+++ control flow tools/4.9-more-on-defining-function/4.9.2-keyword-argument.py
@@ -23,3 +23,12 @@ def function(a):
     pass
 
 function(0, a=0)
+
+def cheeseshop(kind, *arguments, **keywords):
+    print("-- Do you have any", kind, "?")
+    print("-- I'm sorry, we're all out of", kind)
+    for arg in arguments:
+        print(arg)
+    print("-" * 40)
+    for kw in keywords:
+        print(kw, ":", keywords[kw])
