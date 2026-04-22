66f4cc86904239b43d4b6201a0f8bef99e822e85
saurabh <saurabh20tiwari04@gmail.com>
2026-04-22 23:52:38 +0530

    lambda function

---
+++ functions/python-lambda-function.py
@@ -0,0 +1,8 @@
+#Lambda functions are small anonymous functions, meaning they do not have a defined name. These are small, short-lived functions used to pass simple logic to another function.
+
+#contain only one expression 
+#result of that function is retuened automatically 
+
+a = 'saurabh tiwari'
+upper = lambda x: x.upper()  
+print(upper(a))
