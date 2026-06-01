788e41c86fe823f0c0533cfab84fbb267d319bfe
saurabh <saurabh20tiwari04@gmail.com>
2026-06-01 22:51:26 +0530

    invalid age project

--- OOPS/pthon exception handling/user-defined-exception.py
+++ OOPS/pthon exception handling/user-defined-exception.py
@@ -27,3 +27,27 @@ try:
 
 except InvalidAgeError as e:
   print(e)
+
+  
+class InvalidAgeError(Exception):
+  def __init__(self, age , msg="age must be between 0 to 120"):
+    self.age = age
+    self.msg = msg
+    super().__init__(self.msg)  #this calls the parent class for it's method
+
+  def __str__(self):
+    return F'{self.age} -> {self.msg}'
+  
+#use the custom exception in your code 
+def set_age(age):
+  if age < 0 or age > 120:
+    raise InvalidAgeError(age)
+  else:
+    print(f"Age set to: {age}")
+
+# Handling the exception
+try:
+ set_age(150) #this will raise the custom exception 
+
+except InvalidAgeError as e:
+  print(e)
